1a87970d4cc5de43704bdf41b6d26fdab3f8140c
Mamuuut <mamut.delaunay@gmail.com>
2013-03-07 11:17:37 +0100

    can submodule added

---
+++ public/todos/todos.js
@@ -0,0 +1,9 @@
+steal('jquery/class',
+      'jquery/model',
+      'jquery/dom/fixture',
+      'jquery/view/ejs',
+      'jquery/controller',
+      'jquery/controller/route',
+      function($){
+
+})
